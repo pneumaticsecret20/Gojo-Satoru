903bb3f4a24a0002e1c439dc1e5f6fcfd45d59a3
Mississippi <71939220+pneumaticsecret20@users.noreply.github.com>
2022-06-25 12:10:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['254725420543']
+global.premium = ['254757051558']
+global.ownernomer = '254725420543'
+global.ownername = '💗King Mississippi'
+global.botname = 'Cookie-Bot'
+global.footer = '©MspUSAInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Cookie-Satoru'
+global.author = '♈ Mississippi'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🖤','♈','🕊️']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
@@ -43,7 +43,7 @@ global.mess = {
     private: 'Features Used Only For Private Chat!',
     bot: 'This Feature Is Only For Bot',
     wait: 'Wait A minute...',
-    error: 'Error! Maybe Api Key Is Expired🤔!',
+    error: 'Error! Maybe Api Key Is Expired✌🏾!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
     global.limitawal = {
